77bac61dac7b9d9a94547e2adfe7273e6f37b9c6
luiscarloscarlosgonzalez-oss <luiscarloscarlosgonzalez@gmail.com>
2026-02-10 16:28:02 +0000

    Primer Trabajo con Arreglos

---
+++ Seman2/Ejercicio1.java
@@ -0,0 +1,12 @@
+package Seman2;
+
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        //forma1 -> definiendo directamente los valores del arreglo
+        int[] a = {4,8,9,6,1,2};
+
+        for(int i = 0; i < a.length; i++){
+            System.out.println("a["+ i +"]="+ a[i]);
+        }
+    }
+}
