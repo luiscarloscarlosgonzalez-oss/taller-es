5a3d6905c15e7bed2c70584be51e1d1ea9773261
luiscarloscarlosgonzalez-oss <luiscarloscarlosgonzalez@gmail.com>
2026-02-10 17:29:32 +0000

    forma 2 de la declaracion del arreglo

--- Seman2/Ejercicio1.java
+++ Seman2/Ejercicio1.java
@@ -3,10 +3,25 @@ package Seman2;
 public class Ejercicio1 {
     public static void main(String[] args) {
         //forma1 -> definiendo directamente los valores del arreglo
-        int[] a = {4,8,9,6,1,2};
+        int[] a = {4,8,9,6,1,2,10};
 
         for(int i = 0; i < a.length; i++){
             System.out.println("a["+ i +"]="+ a[i]);
         }
+        
+            int par = 0;
+            int impar = 0;
+            for(int i = 0; i < a.length; i++){
+                if (a [i]%2 ==0){
+                    par +=a[i];
+                }else{
+                    impar  +=a[i];
+                   }
+                }
+           
+                System.out.println("la suma numeros pares es =" + par);
+              System.out.println("la suma numeros impar es =" + impar );
+        }
+
     }
-}
+
